bdfcf4e46bde59718592ba3d6b56bc9de40173aa
anakin-dabir <anakindabir@gmail.com>
2024-02-05 10:28:41 +0500

    okay that's it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import AlertDialog from './components/AlertDialog/alertDialog';
+import AppDrawer from './components/AppDrawer/appDrawer';
 
 const App = () => {
   const [open, openSet] = useState(true);
   return (
     <div>
-      <AlertDialog open={open} openSet={openSet} message='Are you sure?' heading='Delete' />
+      <AppDrawer />
     </div>
   );
 };
