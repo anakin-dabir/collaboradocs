2d2deb69e771889940d6aae2f20f9c80b8327bb4
anakin-dabir <anakindabir@gmail.com>
2024-02-07 04:36:23 +0500

    date-pickers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import React, {useState} from 'react';
-import {Stack, useTheme, Dialog} from '@mui/material';
-import {ReactComponent as DividerBoth} from './img/divider_both.svg';
-import XButton from './components/XButton';
-import XStack from './components/XStack';
-import XTopbar from './components/XTopbar';
-import XSortIcon from './components/XSortIcon';
-import XToast from './components/XToast';
-import XChip from './components/XChip';
-import XSwitch from './components/XSwitch';
-import XDivider from './components/XDivider';
-import XFileUpload from './components/XFileUpload';
-import PageHeader from './components/PageHeader/pageHeader';
+import {Stack, useTheme} from '@mui/material';
+import XDateRangePicker from './components/XDateRangePicker';
+import XDatePicker from './components/XDatePicker';
 
 const App = () => {
   const [open, openSet] = useState(false);
@@ -18,26 +9,18 @@ const App = () => {
   const [loading, loadingSet] = useState(false);
   const [file, fileSet] = useState(null);
   const [str, strSet] = useState('');
-  const [selectedDate, selectedDateSet] = useState(['', '']);
+  const [selectedDate, selectedDateSet] = useState('');
+  const [selectedDate1, selectedDateSet1] = useState(['', '']);
 
   console.log(file);
   return (
     <>
       {/* <XTopbar /> */}
       <div className=' h-screen pt-10 bg-black/90 !shadow-2xl'>
-        <PageHeader
-          title={`${'Promotions_page_title'}`}
-          selectedDate={selectedDate}
-          selectedDateSet={selectedDateSet}
-          addBtnText={`${'Promotions_table_add_button'}`}
-          addBtnHandle={() => {
-            openSet(true);
-          }}
-          searchPlaceholder={'Filter chain | brand | product...'}
-          search={str}
-          multipleFilters={true}
-          secondPlaceholder='Filter promo time frame...'
-        />
+        <div className='ml-20'>
+          <XDateRangePicker selectedDate={selectedDate1} selectedDateSet={selectedDateSet1} />
+          <XDatePicker selectedDate={selectedDate} selectedDateSet={selectedDateSet} />
+        </div>
         {/* <XStack className='w-full mt-3 h-20' /> */}
         {/* <div className='mt-4 pl-10'>
           <XStack className='p-4 h-96 w-[960px]'>
@@ -45,24 +28,7 @@ const App = () => {
             <XButton>I Love You</XButton>
           </XStack>
         </div> */}
-        <div className='mt-4 ml-10'>
-          <XSortIcon down />
-        </div>
-        <XChip className='w-32' onDelete={() => openSet(pre => !pre)} />
-        <div className='w-52'>
-          <XDivider />
-        </div>
-        <XSwitch checked={open} onChange={() => openSet(pre => !pre)} disabled />
-
-        <div className='w-96 gap-10'>
-          <XFileUpload
-            className='ml-10'
-            fileSet={fileSet}
-            src={file ? URL.createObjectURL(file) : ''}
-          />
-        </div>
       </div>
-      <XToast isOpen={open} onClose={() => openSet(false)} severity='error' />
     </>
   );
 };
---
+++ src/components/XDatePicker.jsx
@@ -0,0 +1,91 @@
+import React, {useEffect, useRef, useState} from 'react';
+import {IconButton, InputAdornment} from '@mui/material';
+import {IMaskInput} from 'react-imask';
+import {LocalizationProvider, DatePicker} from '@mui/x-date-pickers-pro';
+import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
+import {ReactComponent as CalendarIcon} from '../img/calendarIcon.svg';
+import menu_top_left from '../img/menu_top_left.svg';
+import menu_top_right from '../img/menu_top_right.svg';
+import menu_bottom_right from '../img/menu_bottom_right.svg';
+import menu_bottom_left from '../img/menu_bottom_left.svg';
+import XTextfield from './XTextfield';
+import clsx from 'clsx';
+// import {FormikProps} from 'formik';
+
+export default function XDatePicker({
+  selectedDate = '12/11/2021',
+  selectedDateSet,
+  className = '',
+}) {
+  const [calenderOpen, calenderOpenSet] = useState(false);
+  const [anchorEl, anchorElSet] = useState(null);
+
+  return (
+    <div className={clsx('w-72', className)}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DatePicker
+          value={selectedDate}
+          inputFormat='MM/dd/yyyy'
+          disableMaskedInput
+          PopperProps={{
+            anchorEl,
+            sx: {
+              '&.MuiPickersPopper-root': {
+                backgroundColor: 'rgba(7, 50, 79, 0.92) !important',
+                background: `
+                                  url(${menu_top_left}) top left,
+                                  url(${menu_top_right}) top right,
+                                  url(${menu_bottom_right}) bottom right,
+                                  url(${menu_bottom_left}) bottom left`,
+                backgroundRepeat: 'no-repeat',
+                clipPath: `polygon(12px 0, 100% 0, 100% calc(100% - 12px), calc(100% - 12px) 100%, 0 100%, 0 12px)`,
+              },
+            },
+          }}
+          open={calenderOpen}
+          onClose={() => calenderOpenSet(false)}
+          onOpen={() => calenderOpenSet(true)}
+          onChange={value => {
+            selectedDateSet?.(
+              value?.toLocaleDateString('en-US', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+              }) || ''
+            );
+          }}
+          renderInput={() => (
+            <XTextfield
+              placeholder='DD/MM/YYYY'
+              value={new Date(selectedDate).toLocaleDateString('en-GB')}
+              type='text'
+              inputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <IconButton
+                      onClick={e => {
+                        anchorElSet(e.target);
+                        calenderOpenSet(true);
+                      }}
+                    >
+                      <CalendarIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+                inputProps: {
+                  mask: '00/00/0000',
+                },
+                inputComponent: DateMask,
+              }}
+            />
+          )}
+        />
+      </LocalizationProvider>
+    </div>
+  );
+}
+
+const DateMask = React.forwardRef((props, ref) => {
+  const {mask, ...inputProps} = props;
+  return <IMaskInput {...inputProps} inputRef={ref} mask={mask} />;
+});
