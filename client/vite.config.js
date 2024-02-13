9a4ec1555c5979015d262487ae87408abbfba06e
anakin-dabir <anakindabir@gmail.com>
2024-02-13 13:57:00 +0500

    checking

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import svgr from '@svgr/rollup';
-import url from '@rollup/plugin-url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "@svgr/rollup";
+import url from "@rollup/plugin-url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), url(), svgr()],
+  base: "/",
 });
