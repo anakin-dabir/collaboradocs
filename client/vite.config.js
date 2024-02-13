114374fbc5614cfad7d5328b3db062c2b59c0ef1
anakin-dabir <anakindabir@gmail.com>
2024-02-13 15:01:03 +0500

    let's see

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "@svgr/rollup";
-import url from "@rollup/plugin-url";
+// import url from "@rollup/plugin-url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    url({
-      output: "dist/assets",
-    }),
+    // url({
+    //   output: "dist/assets",
+    // }),
     svgr(),
   ],
   base: "/",
   resolve: {
     alias: { "@": "/src" },
   },
-  assetsInclude: ["src/assets/**/*"],
 });
