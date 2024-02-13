49ae65c8fd5734a0d5df5b66bd59e149729a2650
anakin-dabir <anakindabir@gmail.com>
2024-02-13 15:12:41 +0500

    okay this time

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,47 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "@svgr/rollup";
 // import url from "@rollup/plugin-url";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     // url({
+//     //   output: "dist/assets",
+//     // }),
+//     svgr(),
+//   ],
+//   base: "/",
+//   resolve: {
+//     alias: { "@": "/src" },
+//   },
+// });
+
+import { defineConfig } from "vite";
+import { processAssetFileNames, entryFileNames, chunkFileNames, assetDir } from "./config/assets";
+import { resolve } from "path";
+import viteHtmlResolveAlias from "vite-plugin-html-resolve-alias";
+
 export default defineConfig({
-  plugins: [
-    react(),
-    // url({
-    //   output: "dist/assets",
-    // }),
-    svgr(),
-  ],
-  base: "/",
   resolve: {
-    alias: { "@": "/src" },
+    alias: {
+      "@": "/src",
+      // "@img": resolve(__dirname, "src/arr/"),
+      "@rss": resolve(__dirname, "src/rss.xml"),
+    },
+  },
+  plugins: [react(), svgr(), viteHtmlResolveAlias()],
+  build: {
+    minify: true,
+    assetsDir: assetDir,
+    assetsInlineLimit: 0,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: entryFileNames,
+        assetFileNames: processAssetFileNames,
+        chunkFileNames: chunkFileNames,
+      },
+    },
   },
 });
