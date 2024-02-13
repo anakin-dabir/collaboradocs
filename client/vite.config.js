8ebc991fa618edaccd06c2fa5d5effb1ee5ea966
anakin-dabir <anakindabir@gmail.com>
2024-02-13 14:50:53 +0500

    again okaY

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,16 @@ import url from "@rollup/plugin-url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), url(), svgr()],
+  plugins: [
+    react(),
+    url({
+      output: "dist/assets",
+    }),
+    svgr({ icon: true }),
+  ],
   base: "/",
   resolve: {
     alias: { "@": "/src" },
   },
+  assetsInclude: [],
 });
