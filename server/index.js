6f4526fce556dc4b369965a73a2fd6750078e8b5
anakin-dabir <anakindabir@gmail.com>
2024-02-13 13:00:46 +0500

    Adding Socker.io

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import http from "http";
+import SocketService from "./services/socket.js";
+
+async function init() {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const socket = new SocketService();
+  socket.io.attach(httpServer);
+  app.get("/", (req, res) => res.send("Server running on port 5000"));
+  httpServer.listen(5000, () => console.log("Server running on port", 5000));
+
+  socket.initListeners();
+}
+
+init();
---
+++ server/services/socket.js
@@ -0,0 +1,63 @@
+import { Server } from "socket.io";
+
+class SocketService {
+  io;
+  users;
+
+  constructor() {
+    console.log("Init Socket Service...");
+    this.io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
+    });
+    this.users = [];
+  }
+
+  initListeners() {
+    const io = this.io;
+    console.log("Init Socket Listeners...");
+
+    io.on("connect", (Socket) => {
+      console.log(`New Socket Connected`, Socket.id);
+
+      Socket.on("event:addUser", (userId) => {
+        console.log("user connected", Socket.id);
+        addUser(userId, Socket.id);
+        io.emit("event:getUsers", this.users);
+      });
+
+      Socket.on("event:removeUser", (userId) => {
+        console.log("user removed", Socket.id);
+        removeUser(userId);
+        io.emit("event:getUsers", this.users);
+      });
+
+      // Socket.on("event:message", async ({ message }) => {
+      //   console.log("New Message Rec.", message);
+      // });
+
+      Socket.on("disconnect", () => {
+        console.log("user disconnected");
+        removeUser(Socket.id);
+        io.emit("event:getUsers", this.users);
+      });
+    });
+  }
+
+  addUser(userId, socketId) {
+    !this.users.some((user) => user.userId === userId) && this.users.push({ userId, socketId });
+  }
+
+  removeUser(socketId) {
+    this.users = this.users.filter((user) => user.socketId !== socketId);
+  }
+
+  getUser(userId) {
+    const found = this.users.find((user) => user.userId === userId);
+    return found;
+  }
+}
+
+export default SocketService;
