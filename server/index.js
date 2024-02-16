24d80e07a84055e07d48d5326dcb08c12abffc27
anakin-dabir <anakindabir@gmail.com>
2024-02-16 18:45:53 +0500

    okay !

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ async function init() {
   app.get("/", (req, res) => res.send("Server running on port 5000"));
   app.use("/auth", auth);
 
-  connectDb("mongodb://127.0.0.1:27017/DF-63");
+  connectDb(process.env.MONGO || "mongodb://127.0.0.1:27017/DF-63");
   httpServer.listen(5000, () => console.log("Server running on port", 5000));
   socket.initListeners();
 }
