ecd6fce8ba54853b27a1efae37ae52af723fb9a5
anakin-dabir <anakindabir@gmail.com>
2024-02-18 23:44:11 +0500

    fixning

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import XEditor from "./components/XEditor";
-import XButton from "./components/XButton";
-import {
-  useCreateDocumentMutation,
-  useCreateProjectMutation,
-  useLoginMutation,
-} from "./services/nodeApi";
-import toast from "react-hot-toast";
-import XToast from "./components/XToast";
-import { Stack } from "@mui/material";
 
 const App = () => {
-  const [createDocument] = useCreateDocumentMutation();
-  const [data, dataSet] = useState(null);
-  return (
-    <>
-      <Stack sx={{ height: "100%", width: "100%" }}>
-        <Stack></Stack>
-      </Stack>
-    </>
-  );
+  return <XEditor />;
 };
 
 export default App;
